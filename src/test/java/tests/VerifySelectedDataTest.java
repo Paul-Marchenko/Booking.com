89ed68c1eb060ad27659f6224cc3a9ebba60eba5
Paul-Marchenko <pmarchenko@cogniance.com>
2018-02-15 13:39:22 +0200

    addTest1

--- src/test/java/tests/VerifySelectedDataTest.java
+++ src/test/java/tests/VerifySelectedDataTest.java
@@ -3,8 +3,12 @@ package tests;
 import org.testng.annotations.Test;
 
 public class VerifySelectedDataTest {
-    @Test(priority = 2)
+    @Test(priority = 3)
     public void verifySearchingPlace(){
+
+    }
+    @Test(priority = 3)
+    public void verifycorrectValue(){
 
     }
 }
