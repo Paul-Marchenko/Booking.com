2e213b1b2e14fc519a050d08b143880902623089
Paul-Marchenko <pmarchenko@cogniance.com>
2018-02-16 10:46:35 +0200

    AddNewTest1

--- src/test/java/tests/SelectByCriteriesTest.java
+++ src/test/java/tests/SelectByCriteriesTest.java
@@ -1,4 +1,15 @@
 package tests;
 
+import org.testng.annotations.Test;
+
 public class SelectByCriteriesTest {
+
+    @Test(priority = 3)
+    public void selectByDefinedPrice() {
+
+    }
+    @Test(priority = 3)
+    public void selectByDefinedLevel() {
+
+    }
 }
